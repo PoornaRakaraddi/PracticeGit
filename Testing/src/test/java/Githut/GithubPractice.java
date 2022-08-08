43881eea4bb7031ce2e56cacce3e74355ea75bb9
PoornaRakaraddi <rakaraddipoorna@gmail.com>
2022-08-08 17:22:43 +0530

    Firsteeeee

---
+++ Testing/src/test/java/Githut/GithubPractice.java
@@ -0,0 +1,13 @@
+package Githut;
+
+import org.testng.annotations.Test;
+
+public class GithubPractice {
+	@Test
+	public void practice() {
+		String Browser = System.getProperty("browser");
+		String Url = System.getProperty("url");
+	}
+	
+
+}
